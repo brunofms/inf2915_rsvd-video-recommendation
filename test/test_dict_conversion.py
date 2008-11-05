80fffd6bacdd7d9e1799878ea6ddf4af9424b922
Gustavo Soares <gustavosoares@matrix.glb.com>
2008-11-05 17:23:26 -0200

    teste do dict conversion

---
+++ test/test_dict_conversion.py
@@ -0,0 +1,43 @@
+from numpy.linalg import *
+from numpy import *
+
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+'The Night Listener': 4.5, 'Superman Returns': 4.0,
+'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+#print 'array'
+#a = array(critics)
+#print a
+#print '*' * 50
+
+
+
+
+print critics
+print '*' * 50
+
+matriz = []
+for users in critics.keys():
+	dict_aux = critics[users]
+	lista = []
+	for midias_key in dict_aux.keys():
+		lista.append(dict_aux[midias_key])
+	matriz.append(lista)
+	
+
+print 'matriz'
+print matriz
