77474590e227763eafdebe001170db4706a37b76
Bruno de Figueiredo Melo e Souza <bruno.souza@dl128166.(none)>
2008-11-06 17:10:48 -0200

    Incluisao da busca ao titulo das midias

--- src/itemBasedRecommendation.py
+++ src/itemBasedRecommendation.py
@@ -2,12 +2,29 @@
 
 # Item based recommendation media system
 
-import similarity
-
+import urllib
+from xml.dom import minidom
+from similarity import *
 from logParser import mediaUserDict
+
+URI = 'http://webmedia-api-live.globoi.com/1.0/video/%s'
+
+def getMediaTitle(midia_id):
+	uri = URI % midia_id
+	dom = minidom.parse(urllib.urlopen(uri))
+	media_node = dom.getElementsByTagName("midia")
+
+	return media_node[0].attributes["titulo"].value
+
 
 # TODO: dictionary -> matrix NxM
 # TODO: SVD
 
+
 # TEST: Item similarity dataset
-print similarity.calculateSimilarItems(mediaUserDict)
+mediaDict = calculateSimilarItems(mediaUserDict)
+
+print "838440 - %s" % getMediaTitle(838440)
+
+for similarity, midia_id in mediaDict['838440']:
+	print "%s\t%s" % (midia_id, getMediaTitle(int(midia_id)))
--- src/logParser.py
+++ src/logParser.py
@@ -114,10 +114,11 @@ for line in fileinput.input("../data/logs_flashvideo/access.Nov022008.riols29"):
 		# retrieving info
 		user = hashlib.md5(result['ip_address'] + result['user_agent']).hexdigest().strip()
 		media = result['midia_id'].strip()
-		downloaded = float(result['return_byte'].strip())
-		size = float(getMediaFileSize(result['file_path']))
+#		downloaded = float(result['return_byte'].strip())
+#		size = float(getMediaFileSize(result['file_path']))
 
-		view_rate = downloaded/size
+#		view_rate = downloaded/size
+		view_rate = 1
 
 		# re-generated video adds noise to the dataset
 		if view_rate <= 1:
