a251a0c45c42790f194b0640b53d31598600b9ba
Bruno de Figueiredo Melo e Souza <bruno.souza@dl128166.(none)>
2008-11-17 09:39:25 -0200

    Criando o dataset com usuario, media_id e ratings

--- test/getMediaRest.py
+++ test/getMediaRest.py
@@ -3,13 +3,16 @@ from xml.dom import minidom
 
 URI = 'http://webmedia-api-live.globoi.com/1.0/video/%s'
 
-def getMediaTitle(midia_id):
+def getMediaAverageRating(midia_id):
 	uri = URI % midia_id
 	dom = minidom.parse(urllib.urlopen(uri))
-	media_node = dom.getElementsByTagName("midia")
+	media_node = dom.getElementsByTagName("avaliacao")
 
 #	for node in media_node[0]:
-	print media_node[0].attributes["titulo"].value
+	soma = media_node[0].attributes["somaVotos"].value
+	qde = media_node[0].attributes["quantidadeVotos"].value
+
+	return float(soma)/float(qde)
 
 
-getMediaTitle(838440)
+print "%f" % (getMediaAverageRating(838440))
