f8facbaf7dea34b65ba9b1a5b19349b9ef061a13
Bruno de Figueiredo Melo e Souza <bruno.souza@dl128166.(none)>
2008-11-18 21:46:04 -0200

    Implementacao do algoritmo para validacao do treinamento com base em um dataset de testes

--- test/testPrediction.py
+++ test/testPrediction.py
@@ -1,30 +1,29 @@
+from math import *
+
 # test dataset file path
 TEST_DATASET_FILE=''
 
-# w*q
-predicted_ratings=
-
 # Returns the RMSE of the proposed train process
-def testData()
+def testData(w,q): 
 	i=0
     # le o dataset de testes
 	for line in fileinput.input(TEST_DATASET_FILE):
 		try:
 			line.strip()
-			user, media, rating = line.split('\t')
+			user,media,rating = line.split('\t')
 			user = user.strip()
 			media = media.strip()
 			rating = int(rating.strip())
 
-			if predicted_ratings.has_key(user) and predicted_ratings[user].has_key(media):
-				predicted=int(predicted_ratings[user][media].strip)
-				err+=(rating-predicted)**2
-				i++
+			if w.has_key(user) and q.has_key(media):
+				predicted = int(w[user]*q[media])
+				err = err+(rating-predicted)**2
+				i += 1
 
 		except Exception, why:
 			pass
 
-	mse=err/i
-	rmse=sqrt(mse)
+	mse = err/i
+	rmse = sqrt(mse)
 
 	return rmse
